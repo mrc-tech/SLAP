f6e5483089444c460a39094090350d282da39ad4
Mark <74192712+mrc-tech@users.noreply.github.com>
2023-05-31 12:47:11 +0200

    Update SLAP.h
    added initial header file structure

---
+++ include/SLAP.h
@@ -0,0 +1,10 @@
+/*
+  Simple Linear Algebra Package (SLAP)
+ */
+
+// CORE:
+#include "datatype.h" // matrix and other data type and menagement
+#include "basicops.h" // basic operations
+
+// OPERATIONS:
+
