339dfe21202d6aa4379331038e645529e1ad54cf
Mark <diescc@gmail.com>
2023-07-05 12:24:17 +0200

    BASIC
    - equality
    - transpose

--- include/basicops.h
+++ include/basicops.h
@@ -5,6 +5,9 @@
 #ifndef SLAP_BASICOPS
 #define SLAP_BASICOPS
 
+#include <math.h>
+
+
 void print_mat(matd matrix)
 {
 	// FARE IN MODO CHE STAMPA COME UNA TABELLA PREORDINATA DAL NUMERO DELLE CIFRE (tutto compatto)
@@ -18,11 +21,34 @@ void print_mat(matd matrix)
 }
 
 
+short is_equal(matd m1, matd m2, double tolerance)
+{
+	// return 1 if m1 = m2, else returns 0
+	int i;
+	if((m1.n_rows != m2.n_rows) || (m1.n_cols != m2.n_cols)) return 0; // check dimensions
+	for(i=0; i<m1.n_rows*m1.n_cols; i++){
+		if(fabs(m1.data[i] - m2.data[i] > tolerance)) return 0;
+	}
+	return 1;
+}
+
+
+matd* matd_transpose(matd matrix)
+{
+	matd * m = new_matd(matrix.n_cols, matrix.n_rows); // return matrix
+	int r,c;
+	for(r=0; r<m->n_rows; r++){
+		for(c=0; c<m->n_cols; c++){
+			m->data[c * m->n_cols + r] = matrix.data[r * matrix.n_cols + c];
+		}
+	}
+	
+	return m;
+}
+
 
 /*
 	TODO:
-		- transpose
-		- equality
 		- sum
 		- multiply
 		- extract row/column
