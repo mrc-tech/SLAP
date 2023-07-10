276542f59ffcd62dd11b0bc611d1ded17b3df6d5
Mark <diescc@gmail.com>
2023-07-10 16:32:21 +0200

    folders
    Core folder

--- include/SLAP.h
+++ include/SLAP.h
@@ -7,14 +7,14 @@
 
 
 // INITIAL DEFs:
-#include "defs.h"
+#include "core/defs.h"
 
 // CORE:
-#include "datatype.h" // matrix and other data type and menagement
-#include "basicops.h" // basic operations
+#include "core/datatype.h" // matrix and other data type and menagement
+#include "core/basicops.h" // basic operations
 
 // OPERATIONS:
-#include "strmod.h" // matrix structure modification
+#include "core/strmod.h" // matrix structure modification
 #include "solvers/Gauss.h"
 #include "solvers/LUp.h" // LU(P) factorialization with partial pivoting
 
